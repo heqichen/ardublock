87e357f6c17005d3721c7dd72858bc7367a725f6
HE Qichen <heqichen@gmail.com>
2015-07-07 16:59:29 +0800

    now can check version

--- src/main/java/com/ardublock/core/Updater.java
+++ src/main/java/com/ardublock/core/Updater.java
@@ -1,26 +1,68 @@
 package com.ardublock.core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Updater
 {
+	
+	private ResourceBundle uiMessageBundle;
+	int internalVersion;
+	String os;
+	String machineId;
+	String lang;
+	String queryUrl;
+	public Updater()
+	{
+		uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
+		internalVersion = Integer.parseInt(uiMessageBundle.getString("ardublock.ui.version-internal"));
+		os = getOsInfo();
+		machineId = getMachineId();
+		lang = getLocale();
+		
+		queryUrl = "http://ardublock.heqichen.cn/version.php?mid=" + machineId + "&v=" + internalVersion + "&os=" + os + "&lang=" + lang;
+	}
+	
+	public void startCheck(String action) throws IOException
+	{
+		
+		URL url = new URL(queryUrl + "&action=" + action);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "ardublock");
+		int responseCode = con.getResponseCode();
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		
+		in.close();
+		System.out.println(response.toString());
+		
+	}
+	
 	public static void main(String args[]) throws Exception
 	{
-		ResourceBundle uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
-		
-		int internalVersion = Integer.parseInt(uiMessageBundle.getString("ardublock.ui.version-internal"));
-		
-		
 		Updater u = new Updater();
-		System.out.println(u.getOsInfo());
-		System.out.println(u.getMachineId());
-		System.out.println(internalVersion);
-		System.out.println(u.getLocale());
+		u.startCheck("test");
 	}
+	
+	
+	
 	
 	private String getOsInfo()
 	{
