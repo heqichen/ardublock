6e899307c5c65fb4abeda4a542d65181f940289f
HE Qichen <heqichen@gmail.com>
2015-07-09 00:56:10 +0800

    now can auto create property file

--- src/main/java/com/ardublock/Configuration.java
+++ src/main/java/com/ardublock/Configuration.java
@@ -1,6 +1,8 @@
 package com.ardublock;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 import processing.app.Preferences;
 
@@ -11,21 +13,33 @@ public class Configuration
 	public Configuration(Context context)
 	{
 		this.context = context;
-		configFile = loadConfigFile();
+		File configPath = loadConfigFile();
+		Properties props = new Properties();
+		configFile = new File(configPath.getAbsolutePath(), "ardublock.properties");
+		if (!configFile.isFile())
+	    {
+			try
+			{
+				configFile.createNewFile();
+			} 
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+	    }
+		
 	}
 	
 	private File loadConfigFile()
 	{
 		if (context.isInArduino())
 		{
-			System.out.println("in arduino, sketchbook path: ");
-			System.out.println(Preferences.get("sketchbook.path"));
+			return new File(Preferences.get("sketchbook.path"));
 		}
 		else
 		{
-			System.out.println("not in arduino");
+			return new File(System.getProperty("user.home") + File.separator + "sketchbook");
 		}
-		return null;
 	}
 
 }
